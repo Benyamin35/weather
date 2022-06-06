06e6d53f853e5f92a052a6ac368560bc0e6f8e80
Sycoms <77230943+Benyamin35@users.noreply.github.com>
2022-06-06 15:53:05 +0400

    init

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const button = document.querySelector(".button");
-const input = document.getElementById("inputValue").value;
-console.log(input)
+const input = document.getElementById("inputValue");
+
 button.addEventListener('click', function (){
-fetch("https://api.openweathermap.org/data/2.5/weather?q="+input+"&appid=e3485ab3e409e261d1828f32d0cc44f0")
+      
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=e3485ab3e409e261d1828f32d0cc44f0')
 .then (function (resp) {return resp.json() }) 
 .then (function (data){ 
-    console.log(data);})
+    console.log(data)})
 
 .catch (err => alert ("Wrong city name!"))})
